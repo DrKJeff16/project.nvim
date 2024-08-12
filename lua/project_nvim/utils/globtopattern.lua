048fd2f373d6134265aebcab0b0b1ccb5d598875
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-08-12 16:12:07 -0600

    chore: Added Vim comments on top of all lua files.
    Since indentation is variable across users, make sure to
    enforce it when opening Lua files using (Neo)Vim.
    
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project_nvim/utils/globtopattern.lua
+++ lua/project_nvim/utils/globtopattern.lua
@@ -1,3 +1,5 @@
+-- vim:ts=2:sts=2:sw=2:et:ai:si:sta:
+
 -- Credits for this module goes to: David Manura
 -- https://github.com/davidm/lua-glob-pattern
 
--- lua/project_nvim/utils/history.lua
+++ lua/project_nvim/utils/history.lua
@@ -1,3 +1,5 @@
+-- vim:ts=2:sts=2:sw=2:et:ai:si:sta:
+
 ---@alias OpenMode
 ---|integer
 ---|"a"
