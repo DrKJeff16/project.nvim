1524b04f29fbf6adf0b97caf3d11d07615ffd3e8
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-20 14:31:02 -0600

    Revert "feat: Added entry points for submodules."
    This reverts commit 9a0ad6b049f4363339017b5c78f537fd443d4cb9.

--- lua/project_nvim/init.lua
+++ lua/project_nvim/init.lua
@@ -1,32 +1,15 @@
----@diagnostic disable:missing-fields
-
 local config = require('project_nvim.config')
-local globpattern = require('project_nvim.utils.globpattern')
 local history = require('project_nvim.utils.history')
-local path = require('project_nvim.utils.path')
-
----@class Project.Utils
----@field globpattern Project.Utils.GlobPattern
----@field history Project.Utils.History
----@field path Project.Utils.Path
 
 ---@class Project
----@field config Project.Config
----@field utils Project.Utils
 ---@field setup fun(options: Project.Config.Options)
 ---@field get_recent_projects fun(): string[]
 
 ---@type Project
+---@diagnostic disable-next-line:missing-fields
 local M = {}
 
 M.setup = config.setup
 M.get_recent_projects = history.get_recent_projects
-
-M.config = config
-
-M.utils = {}
-M.utils.history = history
-M.utils.path = path
-M.utils.globpattern = globpattern
 
 return M
