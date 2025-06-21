ed58744173554e87e352486b52588f8146ca6fbf
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-21 00:33:40 -0600

    fix(path): Attempt to make method work.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project_nvim/utils/path.lua
+++ lua/project_nvim/utils/path.lua
@@ -10,7 +10,7 @@ local HISTFILE = string.format('%s/project_history', PROJECTPATH)
 ---@field projectpath string
 ---@field historyfile string
 ---@field init fun(self: Project.Utils.Path)
----@field create_scaffolding fun(self: Project.Utils.Path, callback: (fun(err: string|nil, success: boolean|nil))?)
+---@field create_scaffolding fun(self: Project.Utils.Path, callback: (fun(err: string|nil, success: boolean|nil))?): uv.uv_fs_t
 ---@field is_excluded fun(dir: string): boolean
 ---@field exists fun(path: string): boolean
 
@@ -22,15 +22,10 @@ M.datapath = DATAPATH -- directory
 M.projectpath = PROJECTPATH -- directory
 M.historyfile = HISTFILE -- file
 
-function M:init()
-    self.datapath = require('project_nvim.config').options.datapath or DATAPATH
-    self.projectpath = string.format('%s/project_nvim', self.datapath) -- directory
-    self.historyfile = string.format('%s/project_history', self.projectpath) -- file
-end
-
 ---@param callback fun(err: string|nil, success: boolean|nil)
+---@return uv.uv_fs_t
 function M:create_scaffolding(callback)
-    uv.fs_mkdir(self.projectpath, 448, callback ~= nil and callback or nil)
+    return uv.fs_mkdir(self.projectpath, tonumber('644', 8), callback)
 end
 
 ---@param dir string
@@ -48,5 +43,13 @@ end
 ---@param path string
 ---@return boolean
 function M.exists(path) return vim.fn.empty(vim.fn.glob(path)) == 0 end
+
+function M:init()
+    self.datapath = require('project_nvim.config').options.datapath or DATAPATH
+    self.projectpath = string.format('%s/project_nvim', self.datapath) -- directory
+    self.historyfile = string.format('%s/project_history', self.projectpath) -- file
+
+    self:create_scaffolding()
+end
 
 return M
