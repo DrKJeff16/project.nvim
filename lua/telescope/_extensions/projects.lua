de14c1072883b75b98d291ceed92e7b8053c71de
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-29 23:49:11 -0600

    refactor(telescope): Reworked some sections for the `telescope` picker.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/telescope/_extensions/projects.lua
+++ lua/telescope/_extensions/projects.lua
@@ -1,51 +1,77 @@
 -- Inspiration from:
 -- https://github.com/nvim-telescope/telescope-project.nvim
-local has_telescope, telescope = pcall(require, 'telescope')
+local has_telescope, _ = pcall(require, 'telescope')
 
 if not has_telescope then
     return
 end
 
-local finders = require('telescope.finders')
-local pickers = require('telescope.pickers')
-local telescope_config = require('telescope.config').values
-local actions = require('telescope.actions')
-local state = require('telescope.actions.state')
-local builtin = require('telescope.builtin')
-local entry_display = require('telescope.pickers.entry_display')
+local Telescope = require('telescope')
 
-local history = require('project_nvim.utils.history')
-local project = require('project_nvim.project')
-local config = require('project_nvim.config')
+local Finders = require('telescope.finders')
+local Pickers = require('telescope.pickers')
+local Actions = require('telescope.actions')
+local State = require('telescope.actions.state')
+local Builtin = require('telescope.builtin')
+local Entry_display = require('telescope.pickers.entry_display')
+
+local telescope_config = require('telescope.config').values
+
+local History = require('project_nvim.utils.history')
+local Project = require('project_nvim.project')
+local Config = require('project_nvim.config')
 
 ----------
 -- Actions
 ----------
 
+---@param t 'nil'|'number'|'string'|'boolean'|'table'|'function'|'thread'|'userdata'
+---@param data any
+---@return boolean
+local function is_type(t, data)
+    local types = {
+        'nil',
+        'number',
+        'string',
+        'boolean',
+        'table',
+        'function',
+        'thread',
+        'userdata',
+    }
+
+    if t == nil or type(t) ~= 'string' or not vim.tbl_contains(types, t) then
+        return false
+    end
+
+    -- `nil` is a special case
+    if t == 'nil' then
+        return data == nil
+    end
+
+    return (data ~= nil and type(data) == t)
+end
+
 ---@return table
 local function create_finder()
-    local results = history.get_recent_projects()
+    local results = History.get_recent_projects()
+
+    local len = #results
 
     -- Reverse results
-    for i = 1, math.floor(#results / 2) do
-        results[i], results[#results - i + 1] = results[#results - i + 1], results[i]
+    for i = 1, math.floor(len / 2) do
+        results[i], results[len - i + 1] = results[len - i + 1], results[i]
     end
-    local displayer = entry_display.create({
+
+    local displayer = Entry_display.create({
         separator = ' ',
-        items = {
-            {
-                width = 30,
-            },
-            {
-                remaining = true,
-            },
-        },
+        items = { { width = 30 }, { remaining = true } },
     })
 
     ---@param entry table
     local function make_display(entry) return displayer({ entry.name, { entry.value, 'Comment' } }) end
 
-    return finders.new_table({
+    return Finders.new_table({
         results = results,
         entry_maker = function(entry)
             local name = vim.fn.fnamemodify(entry, ':t')
@@ -60,82 +86,82 @@ local function create_finder()
 end
 
 ---@param prompt_bufnr integer
----@param prompt boolean
 ---@return unknown|nil,boolean?
-local function change_working_directory(prompt_bufnr, prompt)
-    local selected_entry = state.get_selected_entry(prompt_bufnr)
-    if selected_entry == nil then
-        actions.close(prompt_bufnr)
+local function change_working_directory(prompt_bufnr)
+    local selected_entry = State.get_selected_entry()
+
+    Actions.close(prompt_bufnr)
+
+    if is_type('nil', selected_entry) then
         return
     end
+
     local project_path = selected_entry.value
 
-    -- FIXME: `_close()` is deprecated!!! (what did this conditional do beforehand????)
-    actions.close(prompt_bufnr)
-    local cd_successful = project.set_pwd(project_path, 'telescope')
+    local cd_successful = Project.set_pwd(project_path, 'telescope')
     return project_path, cd_successful
 end
 
 ---@param prompt_bufnr integer
 local function find_project_files(prompt_bufnr)
-    local project_path, cd_successful = change_working_directory(prompt_bufnr, true)
+    local project_path, cd_successful = change_working_directory(prompt_bufnr)
     local opt = {
         cwd = project_path,
-        hidden = config.options.show_hidden,
+        hidden = Config.options.show_hidden,
         mode = 'insert',
     }
     if cd_successful then
-        builtin.find_files(opt)
+        Builtin.find_files(opt)
     end
 end
 
 ---@param prompt_bufnr integer
 local function browse_project_files(prompt_bufnr)
-    local project_path, cd_successful = change_working_directory(prompt_bufnr, true)
+    local project_path, cd_successful = change_working_directory(prompt_bufnr)
     local opt = {
         cwd = project_path,
-        hidden = config.options.show_hidden,
+        hidden = Config.options.show_hidden,
     }
     if cd_successful then
-        builtin.file_browser(opt)
+        Builtin.file_browser(opt)
     end
 end
 
 ---@param prompt_bufnr integer
 local function search_in_project_files(prompt_bufnr)
-    local project_path, cd_successful = change_working_directory(prompt_bufnr, true)
+    local project_path, cd_successful = change_working_directory(prompt_bufnr)
     local opt = {
         cwd = project_path,
-        hidden = config.options.show_hidden,
+        hidden = Config.options.show_hidden,
         mode = 'insert',
     }
     if cd_successful then
-        builtin.live_grep(opt)
+        Builtin.live_grep(opt)
     end
 end
 
 ---@param prompt_bufnr integer
 local function recent_project_files(prompt_bufnr)
-    local _, cd_successful = change_working_directory(prompt_bufnr, true)
+    local _, cd_successful = change_working_directory(prompt_bufnr)
     local opt = {
         cwd_only = true,
-        hidden = config.options.show_hidden,
+        hidden = Config.options.show_hidden,
     }
     if cd_successful then
-        builtin.oldfiles(opt)
+        Builtin.oldfiles(opt)
     end
 end
 
 ---@param prompt_bufnr integer
 local function delete_project(prompt_bufnr)
-    local selectedEntry = state.get_selected_entry(prompt_bufnr)
-    if selectedEntry == nil then
-        actions.close(prompt_bufnr)
+    local active_entry = State.get_selected_entry()
+    if is_type('nil', active_entry) then
+        Actions.close(prompt_bufnr)
         return
     end
 
     local choice = vim.fn.confirm(
-        string.format("Delete '%s' from project list?", selectedEntry.value),
+        string.format("Delete '%s' from project list?", active_entry.value),
         '&Yes\n&No',
         2
     )
@@ -144,48 +170,46 @@ local function delete_project(prompt_bufnr)
         return
     end
 
-    history.delete_project(selectedEntry)
+    History.delete_project(active_entry)
 
     local finder = create_finder()
-    state.get_current_picker(prompt_bufnr):refresh(finder, {
+    State.get_current_picker(prompt_bufnr):refresh(finder, {
         reset_prompt = true,
     })
 end
 
----Main entrypoint for Telescope.
+-- Main entrypoint for Telescope
 ---@param opts table
 local function projects(opts)
-    opts = opts or {}
+    opts = is_type('table', opts) and opts or {}
 
-    pickers
-        .new(opts, {
-            prompt_title = 'Recent Projects',
-            finder = create_finder(),
-            previewer = false,
-            sorter = telescope_config.generic_sorter(opts),
-            ---@param prompt_bufnr integer
-            ---@param map fun(mode: string, lhs: string, rhs: string|fun())
-            attach_mappings = function(prompt_bufnr, map)
-                map('n', 'f', find_project_files)
-                map('n', 'b', browse_project_files)
-                map('n', 'd', delete_project)
-                map('n', 's', search_in_project_files)
-                map('n', 'r', recent_project_files)
-                map('n', 'w', change_working_directory)
+    Pickers.new(opts, {
+        prompt_title = 'Recent Projects',
+        finder = create_finder(),
+        previewer = false,
+        sorter = telescope_config.generic_sorter(opts),
+        ---@param prompt_bufnr integer
+        ---@param map fun(mode: string, lhs: string, rhs: string|fun())
+        attach_mappings = function(prompt_bufnr, map)
+            map('n', 'f', find_project_files)
+            map('n', 'b', browse_project_files)
+            map('n', 'd', delete_project)
+            map('n', 's', search_in_project_files)
+            map('n', 'r', recent_project_files)
+            map('n', 'w', change_working_directory)
 
-                map('i', '<c-f>', find_project_files)
-                map('i', '<c-b>', browse_project_files)
-                map('i', '<c-d>', delete_project)
-                map('i', '<c-s>', search_in_project_files)
-                map('i', '<c-r>', recent_project_files)
-                map('i', '<c-w>', change_working_directory)
+            map('i', '<c-f>', find_project_files)
+            map('i', '<c-b>', browse_project_files)
+            map('i', '<c-d>', delete_project)
+            map('i', '<c-s>', search_in_project_files)
+            map('i', '<c-r>', recent_project_files)
+            map('i', '<c-w>', change_working_directory)
 
-                local on_project_selected = function() find_project_files(prompt_bufnr) end
-                actions.select_default:replace(on_project_selected)
-                return true
-            end,
-        })
-        :find()
+            local on_project_selected = function() find_project_files(prompt_bufnr) end
+            Actions.select_default:replace(on_project_selected)
+            return true
+        end,
+    }):find()
 end
 
-return telescope.register_extension({ exports = { projects = projects } })
+return Telescope.register_extension({ exports = { projects = projects } })
