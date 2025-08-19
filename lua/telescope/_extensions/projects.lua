800bab421f9b266623bf2700039adc099e49889b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-08-19 14:50:34 -0600

    chore(telescope): Renamed identifiers.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/telescope/_extensions/projects.lua
+++ lua/telescope/_extensions/projects.lua
@@ -8,8 +8,11 @@ end
 local Telescope = require('telescope')
 local Main = require('telescope._extensions.projects.main')
 
+local setup = Main.setup
+local projects = Main.projects
+
 return Telescope.register_extension({
-    setup = Main.setup,
-    exports = { projects = Main.projects },
-    projects = Main.projects,
+    setup = setup,
+    exports = { projects = projects },
+    projects = projects,
 })
