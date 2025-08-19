e2fa4a47becc2eef5b52a8a0542a9805ef730cd4
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-08-18 18:35:01 -0600

    fix!: Reworded warning for old module. Will delete it 2025-08-21.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project_nvim.lua
+++ lua/project_nvim.lua
@@ -1,9 +1,17 @@
 vim.notify(
     [[
 WARNING: The `project.nvim` module has been renamed to `'project'`.
-Instead of using `require('project_nvim')` use `require('project')`
+
+Instead of using `require('project_nvim')` use `require('project')`.
+====================================================================
+
+          THIS MODULE WILL BE DELETED ON 2025-08-21.
 ]],
     vim.log.levels.WARN
 )
 
-return require('project')
+local lazy = require('project.lazy')
+
+local Project = lazy.require('project') ---@module 'project'
+
+return Project
