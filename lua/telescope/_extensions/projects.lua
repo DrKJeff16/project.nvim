fe4af98180b8faba266faf36e14e52fe30307f88
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-02 18:29:44 -0600

    chore(telescope): cleaned pointless variables
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/telescope/_extensions/projects.lua
+++ lua/telescope/_extensions/projects.lua
@@ -1,13 +1,10 @@
 local Main = require('telescope._extensions.projects.main')
 local Telescope = require('telescope')
 
-local setup = Main.setup
-local projects = Main.projects
-
 return Telescope.register_extension({
-    setup = setup,
-    exports = { projects = projects },
-    projects = projects,
+    setup = Main.setup,
+    exports = { projects = Main.projects },
+    projects = Main.projects,
 })
 
 -- vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:
