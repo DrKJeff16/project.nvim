063f9a2fe9cebac1e8752638439ece87e70ad253
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-05 19:18:59 -0600

    revert: "fix: disabled debug notifications"
    This reverts commit 87cd84e8e1885fa961bfd336bbcc28ae6302986d.

--- lua/project/utils/path.lua
+++ lua/project/utils/path.lua
@@ -78,6 +78,7 @@ end
 ---@return string|'/'
 function Path.get_parent(path_str)
     path_str = path_str:match('^(.*)/')
+    vim.notify(fmt('Parent: %s', path_str or '/'))
     return (path_str ~= '') and path_str or '/' ---@cast path_str  string
 end
 
