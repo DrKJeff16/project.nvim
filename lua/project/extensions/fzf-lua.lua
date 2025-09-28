7b8cc1bf44e98ba20aef97468d495200f3ea8e26
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-27 19:17:16 -0600

    chore: code cleanup
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project/extensions/fzf-lua.lua
+++ lua/project/extensions/fzf-lua.lua
@@ -1,29 +1,31 @@
 local ERROR = vim.log.levels.ERROR
 local Util = require('project.utils.util')
-local reverse = Util.reverse
 local mod_exists = Util.mod_exists
 
 ---@class Project.Extensions.FzfLua
 local M = {}
 
----@param selected table
+---@param selected string[]
 function M.default(selected)
+    local Opts = require('project.config').options
     require('fzf-lua').files({
         cwd = selected[1],
-        silent = require('project.config').options.silent_chdir,
-        hidden = require('project.config').options.show_hidden,
+        silent = Opts.silent_chdir,
+        hidden = Opts.show_hidden,
     })
 end
 
----@param selected table
+---@param selected string[]
 function M.delete_project(selected)
-    require('project.utils.history').delete_project({ value = selected[1] })
+    vim.print(selected)
+    require('project.utils.history').delete_project(selected[1])
 end
 
----@type fun(cb: fun(entry?: string|number, cb?: function))|fun(wnl: fun(entry?: string|number, cb?: function), w: fun(entry?: string|number, cb?: function))
+---@param cb fun(entry?: string|number, cb?: function)
 function M.exec(cb)
-    local results = require('project.utils.history').get_recent_projects()
-    for _, entry in ipairs(reverse(results)) do
+    ---@type string[]
+    local results = Util.reverse(require('project.utils.history').get_recent_projects())
+    for _, entry in ipairs(results) do
         cb(entry)
     end
     cb()
@@ -31,8 +33,7 @@ end
 
 ---This runs assuming you have FZF-Lua installed!
 ---
----CREDITS: @deathmaz
----https://github.com/ahmedkhalf/project.nvim/issues/71#issuecomment-1212993659
+---CREDITS: [@deathmaz](https://github.com/ahmedkhalf/project.nvim/issues/71#issuecomment-1212993659)
 --- ---
 function M.run_fzf_lua()
     if not mod_exists('fzf-lua') then
