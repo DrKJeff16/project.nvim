9876a43821560b8abf40a06abbf63d97a333bda4
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-29 15:08:34 -0600

    feat(mini): structure the bare file
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project/extensions/session/mini.lua
+++ lua/project/extensions/session/mini.lua
@@ -1,8 +1,18 @@
 ---Tentative to `mini.sessions` integration.
+if not _G.MiniSessions then ---@diagnostic disable-line:undefined-field
+    return
+end
 
 local MODSTR = 'project.extensions.session.mini'
-local Api = require('project.api')
+local WARN = vim.log.levels.WARN
 local Log = require('project.utils.log')
-local Utils = require
+local Utils = require('project.utils.util')
+if vim.g.minisessions_disable then
+    Log.warn(('(%s): `mini.sessions` is globally disabled!'):format(MODSTR))
+    vim.notify(('(%s): `mini.sessions` is globally disabled!'):format(MODSTR), WARN)
+    return
+end
+
+local Api = require('project.api')
 
 -- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
