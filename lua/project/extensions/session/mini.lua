7c0484eb942d8d5fe32baac76396834a04b8278b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-29 14:46:15 -0600

    feat(extensions): add bare starting point for `mini.sessions`
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ lua/project/extensions/session/mini.lua
@@ -0,0 +1,8 @@
+---Tentative to `mini.sessions` integration.
+
+local MODSTR = 'project.extensions.session.mini'
+local Api = require('project.api')
+local Log = require('project.utils.log')
+local Utils = require
+
+-- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
