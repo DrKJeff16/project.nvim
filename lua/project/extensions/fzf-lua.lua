b62b6f1d33e5b6a8c8262cdb945469d234581f63
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-12-04 11:47:27 -0600

    fix(fzf-lua): harden module
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project/extensions/fzf-lua.lua
+++ lua/project/extensions/fzf-lua.lua
@@ -3,9 +3,8 @@ local ERROR = vim.log.levels.ERROR
 local Util = require('project.utils.util')
 
 ---@class Project.Extensions.FzfLua
-local FzfLua = {
-    ---@param selected string[]
-    default = function(selected)
+local M = {
+    default = function(selected) ---@param selected string[]
         local Opts = require('project.config').options
         require('project.utils.log').debug(
             ('(%s.default): Running default fzf-lua action.'):format(MODSTR)
@@ -16,18 +15,14 @@ local FzfLua = {
             hidden = Opts.show_hidden,
         })
     end,
-
-    ---@param selected string[]
-    delete_project = function(selected)
+    delete_project = function(selected) ---@param selected string[]
         require('project.utils.log').debug(
             ('(%s.delete_project): Deleting project `%s`'):format(MODSTR, selected[1])
         )
         require('project.utils.history').delete_project(selected[1])
     end,
-    ---@param cb fun(entry?: string|number, cb?: function)
-    exec = function(cb)
-        ---@type string[]
-        local results = Util.reverse(require('project.utils.history').get_recent_projects())
+    exec = function(cb) ---@param cb fun(entry?: string|number, cb?: function)
+        local results = Util.reverse(require('project.utils.history').get_recent_projects()) ---@type string[]
         for _, entry in ipairs(results) do
             cb(entry)
         end
@@ -39,21 +34,29 @@ local FzfLua = {
 ---
 ---CREDITS: [@deathmaz](https://github.com/ahmedkhalf/project.nvim/issues/71#issuecomment-1212993659)
 --- ---
-function FzfLua.run_fzf_lua()
+function M.run_fzf_lua()
     local Log = require('project.utils.log')
     if not Util.mod_exists('fzf-lua') then
         Log.error(('(%s.run_fzf_lua): `fzf-lua` is not installed!'):format(MODSTR))
-        vim.notify(('(%s.run_fzf_lua): `fzf-lua` is not installed!'):format(MODSTR), ERROR)
-        return
+        error(('(%s.run_fzf_lua): `fzf-lua` is not installed!'):format(MODSTR), ERROR)
     end
     Log.info(('(%s.run_fzf_lua): Running `fzf_exec`.'):format(MODSTR))
-    require('fzf-lua').fzf_exec(FzfLua.exec, {
+
+    local Fzf = require('fzf-lua')
+    Fzf.fzf_exec(M.exec, {
         actions = {
-            default = { FzfLua.default },
-            ['ctrl-d'] = { FzfLua.delete_project, require('fzf-lua').actions.resume },
+            default = { M.default },
+            ['ctrl-d'] = { M.delete_project, Fzf.actions.resume },
         },
     })
 end
+
+local FzfLua = setmetatable(M, { ---@type Project.Extensions.FzfLua
+    __index = M,
+    __newindex = function()
+        vim.notify('Project.Extensions.FzfLua is Read-Only!', ERROR)
+    end,
+})
 
 return FzfLua
 -- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
