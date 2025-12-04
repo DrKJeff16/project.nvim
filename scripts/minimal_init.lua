5ef5d52333585ac4d528e7425b83d0a79e77eefd
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-12-04 11:26:21 -0600

    fix(test): no longer ignore `minimal_init.lua`
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ scripts/minimal_init.lua
@@ -0,0 +1,11 @@
+-- Add current directory to 'runtimepath' to be able to use 'lua' files
+vim.cmd("let &rtp.=','.getcwd()")
+
+-- Set up 'mini.test' and 'mini.doc' only when calling headless Neovim (like with `make test` or `make documentation`)
+if vim.tbl_isempty(vim.api.nvim_list_uis()) then
+    -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
+    -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
+    vim.cmd('set rtp+=deps/mini.nvim')
+
+    require('mini.test').setup()
+end
