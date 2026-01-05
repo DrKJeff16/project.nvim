5e878c8755f8414f879024999315bfb3530ec9aa
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-05 13:30:45 -0600

    chore: restructure main module syntax
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project.lua
+++ lua/project.lua
@@ -8,39 +8,40 @@ local Util = require('project.utils.util')
 ---The `project.nvim` module.
 --- ---
 ---@class Project
-local M = {
-  setup = Config.setup,
-  get_config = Config.get_config,
-  get_recent_projects = History.get_recent_projects,
-  delete_project = History.delete_project,
-  get_project_root = Api.get_project_root,
-  get_history_paths = Api.get_history_paths,
-  get_last_project = Api.get_last_project,
-  open_menu = Popup.open_menu,
-  delete_menu = Popup.delete_menu,
-  recents_menu = Popup.recents_menu,
-  session_menu = Popup.session_menu,
-  run_fzf_lua = require('project.extensions.fzf-lua').run_fzf_lua,
-  ---CREDITS: https://github.com/ahmedkhalf/project.nvim/pull/149
-  --- ---
-  ---@param refresh boolean|nil
-  ---@return string|nil curr
-  ---@return string|nil method
-  ---@return string|nil last
-  current_project = function(refresh)
-    Util.validate({ refresh = { refresh, { 'boolean', 'nil' }, true } })
-    refresh = refresh ~= nil and refresh or false
+local M = {}
 
-    local Log = require('project.utils.log')
-    if refresh then
-      Log.debug(('(%s.current_project): Refreshing current project info.'):format(MODSTR))
-      return Api.get_current_project()
-    end
+M.setup = Config.setup
+M.get_config = Config.get_config
+M.get_recent_projects = History.get_recent_projects
+M.delete_project = History.delete_project
+M.get_project_root = Api.get_project_root
+M.get_history_paths = Api.get_history_paths
+M.get_last_project = Api.get_last_project
+M.open_menu = Popup.open_menu
+M.delete_menu = Popup.delete_menu
+M.recents_menu = Popup.recents_menu
+M.session_menu = Popup.session_menu
+M.run_fzf_lua = require('project.extensions.fzf-lua').run_fzf_lua
 
-    Log.debug(('(%s.current_project): Not refreshing current project info.'):format(MODSTR))
-    return Api.current_project, Api.current_method, Api.last_project
-  end,
-}
+---CREDITS: https://github.com/ahmedkhalf/project.nvim/pull/149
+--- ---
+---@param refresh boolean|nil
+---@return string|nil curr
+---@return string|nil method
+---@return string|nil last
+function M.current_project(refresh)
+  Util.validate({ refresh = { refresh, { 'boolean', 'nil' }, true } })
+  refresh = refresh ~= nil and refresh or false
+
+  local Log = require('project.utils.log')
+  if refresh then
+    Log.debug(('(%s.current_project): Refreshing current project info.'):format(MODSTR))
+    return Api.get_current_project()
+  end
+
+  Log.debug(('(%s.current_project): Not refreshing current project info.'):format(MODSTR))
+  return Api.current_project, Api.current_method, Api.last_project
+end
 
 local Project = setmetatable(M, { ---@type Project
   __index = M,
