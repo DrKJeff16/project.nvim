08085e88cb6d0891a6f503b99fc991961020b877
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-08 12:34:12 -0600

    test: use `busted` testing
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ spec/project_setup_spec.lua
@@ -0,0 +1,37 @@
+local assert = require('luassert') ---@type Luassert
+
+describe('project.nvim', function()
+  local project ---@type Project
+
+  before_each(function()
+    -- Clear module cache to get fresh instance
+    package.loaded.project = nil
+    project = require('project')
+  end)
+
+  describe('setup', function()
+    it('should set default configuration', function()
+      local ok = pcall(project.setup)
+      assert.is_true(ok)
+    end)
+
+    it('should merge user configuration with defaults', function()
+      local ok = pcall(project.setup, {})
+      assert.is_true(ok)
+    end)
+
+    it('should handle nil options', function()
+      local ok = pcall(project.setup, nil)
+      assert.is_true(ok)
+    end)
+
+    local params = { 1, false, '', function() end }
+    for _, param in ipairs(params) do
+      it(('should throw error when called with param of type %s'):format(type(param)), function()
+        local ok = pcall(project.setup, param)
+        assert.is_false(ok)
+      end)
+    end
+  end)
+end)
+-- vim: set ts=2 sts=2 sw=2 et ai si sta:
