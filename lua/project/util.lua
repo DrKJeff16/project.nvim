84df1ae586772d40fb5b723a9dfc16bdd376e84a
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-02-03 19:33:28 -0600

    fix(util): correct strip functions
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/project/util.lua
+++ lua/project/util.lua
@@ -359,7 +359,7 @@ function M.lstrip(char, str)
     char = { char, { 'string', 'table' } },
     str = { str, { 'string' } },
   })
-  if str == '' or not vim.startswith(str, char) then
+  if str == '' then
     return str
   end
 
@@ -376,11 +376,15 @@ function M.lstrip(char, str)
     return str
   end
 
+  if not vim.startswith(str, char) or char:len() > str:len() then
+    return str
+  end
+
   ---@cast char string
   local i, len, new_str = 1, str:len(), ''
   local other = false
-  while i <= len + 1 do
-    if str:sub(i, i) ~= char and not other then
+  while i <= len and i + char:len() - 1 <= len do
+    if str:sub(i, i + char:len() - 1) ~= char and not other then
       other = true
     end
     if other then
@@ -388,7 +392,7 @@ function M.lstrip(char, str)
     end
     i = i + 1
   end
-  return new_str
+  return new_str ~= '' and new_str or str
 end
 
 ---Right strip given a leading string (or list of strings) within a string, if any.
@@ -420,12 +424,11 @@ function M.rstrip(char, str)
   end
 
   ---@cast char string
-  str = str:reverse()
-
-  if not vim.startswith(str, char) then
-    return str:reverse()
+  if not vim.startswith(str:reverse(), char) or char:len() > str:len() then
+    return str
   end
-  return M.lstrip(char, str):reverse()
+
+  return M.lstrip(char, str:reverse()):reverse()
 end
 
 ---Strip given a leading string (or list of strings) within a string, if any, bidirectionally.
